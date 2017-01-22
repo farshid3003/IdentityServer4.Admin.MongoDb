ac8caf13477b3eeaea3d24732b1eae4e03c6f4b1
Farşid Mirza <farshid3003@hotmail.com>
2017-01-22 23:22:50 +0300

    first commit

---
+++ src/IdentityServer4.Admin.MongoDb/Startup.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace IdentityServer4.Admin.MongoDb
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationScheme = "Cookies"
+            });
+
+            IdentityConfig idsConfig = Configuration.GetSection("Identity").Get<IdentityConfig>();
+            var options = new OpenIdConnectOptions
+            {
+                AuthenticationScheme = "oidc",
+                SignInScheme = "Cookies",
+
+                Authority = idsConfig.Url,
+                RequireHttpsMetadata = false,
+
+                ClientId = "clientadmin",
+                ClientSecret = "secret",
+
+                ResponseType = idsConfig.ResponseType,
+
+                GetClaimsFromUserInfoEndpoint = true,
+                SaveTokens = true
+            };
+            foreach (var item in idsConfig.Scope)
+            {
+                options.Scope.Add(item);
+            }
+            app.UseOpenIdConnectAuthentication(options); 
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        class IdentityConfig
+        {
+            public string Url { get; set; }
+            public string ResponseType { get; set; }
+            public string[] Scope { get; set; }
+        }
+    }
+}
